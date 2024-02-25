2a76882b536625264e47cec7798fee5d437457cc
maximgf <150217322+maximgf@users.noreply.github.com>
2024-02-25 23:50:02 +0800

    Delete Models directory

--- Models/nomen_grope.py
+++
@@ -1,9 +0,0 @@
-from Src.Models import abstract_referances
-
-
-class nomen_group_model(abstract_referances):
-    
-
-    @staticmethod
-    def create_group():
-        return nomen_group_model('Ингридиенты')
